09778c4e93abe20408f2c4260012f9c652fe6036
Michal Bojanowski <michal2992@gmail.com>
2014-11-05 16:06:41 +0100

    Dane z mapą Warszawy

---
+++ R/Warszawa.R
@@ -0,0 +1,11 @@
+#' Polygons for 18 districts of Warsaw
+#'
+#' Administrative map of Warsaw
+#'
+#'
+#' @format Object of class "spatialPolygonsDataFrame" with some variables to be
+#' described later.
+#'
+#' @source Centralny Ośrodek Dokumentacji Geodezyjnej i Kartograficznej (CODGiK)
+#' \url{codgik.gov.pl}
+"Warszawa"
